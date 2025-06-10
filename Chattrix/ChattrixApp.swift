519009614c45d5b91197559a1f5d8f0c69e3a8ff
Nishant <itsnishant444@gmail.com>
2025-06-10 10:48:52 +0530

    Initial Commit

---
+++ Chattrix/ChattrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattrixApp.swift
+//  Chattrix
+//
+//  Created by Nishant on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ChattrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChattrixTests/ChattrixTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChattrixTests.swift
+//  ChattrixTests
+//
+//  Created by Nishant on 10/06/25.
+//
+
+import Testing
+@testable import Chattrix
+
+struct ChattrixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
